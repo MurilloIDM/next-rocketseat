3f8bce2117d4992a775b7553814442f3f47a0d77
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-08-25 21:55:44 -0300

    New: integração da API do Stripe

---
+++ src/services/strapi.ts
@@ -0,0 +1,12 @@
+import Stripe from "stripe";
+
+export const stripe = new Stripe(
+  process.env.STRIPE_API_KEY,
+  {
+    apiVersion: "2022-08-01",
+    appInfo: {
+      name: "Ig.news",
+      version: "0.1.0"
+    }
+  }
+)
