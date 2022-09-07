3ba48d649f8de4b3a4c5d7b3ca4ac4af951e8e9c
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-09-06 21:09:47 -0300

    New: config faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,12 @@
+import { query as q } from "faunadb";
+
 import nextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
+import { fauna } from "../../../services/fauna";
+
 export default nextAuth({
+  secret: process.env.SIGN_KEY,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -13,4 +18,36 @@ export default nextAuth({
       }
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user?.email),
+                ),
+              ),
+            ),
+            q.Create(
+              q.Collection("users"),
+              { data: { email: user?.email } },
+            ),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user?.email),
+              ),
+            ),
+          ),
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  }
 });
